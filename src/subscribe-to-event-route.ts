b93a654cf0ef8ee0ca369b98bd142c565146d399
GabriellMaiaa <gribeiromaia736@gmail.com>
2025-02-18 21:12:16 -0400

    Adding env file to our project

--- src/subscribe-to-event-route.ts
+++ src/subscribe-to-event-route.ts
@@ -6,6 +6,8 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
     "/subscriptions",
     {
       schema: {
+        summary: "Subscribe to event",
+        tags: ["Subscription"],
         body: z.object({
           name: z.string(),
           email: z.string().email(),
